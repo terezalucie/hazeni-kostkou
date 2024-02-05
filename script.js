639feef233fdbd897fe229c87610e079ee89e3d2
Tereza Kolaříková <tereza.lucie@protonmail.com>
2024-02-05 15:28:57 +0100

    znova

--- script.js
+++ script.js
@@ -3,17 +3,9 @@
 Zamyslete se nad tím, zda vámi vytvořený výraz generuje všechna čísla skutečně se stejnou pravděpodobností. Vemte v úvahu, že funkce Math.random generuje náhodná čísla mezi 0 (včetně) a 1 (vyjma). Je tedy malinká pravěpodobnost, že občas padne přesně číslo 0. Naopak číslo 1 padnout nemůže.
  */
 
-
-/* let nahodneCislo = Math.random()
-let vysledekHodu = Math.floor(nahodneCislo * 6) + 1
-
-let vysledek = vysledekHodu
-console.log("Hod kostkou: " + vysledek) */
-
 let nahodneCislo = Math.random()
 let vysledekHodu = Math.floor(nahodneCislo * 6) + 1
 let vysledek = vysledekHodu
 
 console.log("Hod kostkou: " + vysledek)
-document.body.innerHTML = <h1>("Hod kostkou: " + vysledek)</h1>
-document.body.innerHTML = "Hod kostkou: " + vysledek
+document.body.innerHTML += "Hod kostkou: " + vysledek
